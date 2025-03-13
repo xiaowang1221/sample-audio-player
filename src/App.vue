74a36d01439061deb78f8ba0bf200461089447e2
Xiao Wang <hmw@g.com>
2025-03-13 20:51:49 +0900

    upload v0.4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
   import InterFace from './components/InterFace.vue';
-  import Test from "./components/Test.vue";
 </script>
 
 <template>
   <div>
     <InterFace />
-    <Test />
   </div>
 </template>
 
